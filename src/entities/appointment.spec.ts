bb61ce027ee2bc985b85c9c54b7bf1b5d4ec2784
brunoherdina <brunoherdina@outlook.com>
2023-01-15 11:49:23 -0300

    Refactoring tests to use getFutureDate function.

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,11 +1,10 @@
 import {expect , test} from 'vitest';
 import { Appointment } from './appointment';
+import { getFutureDate} from '../tests/utils/get-future-date';
 
 test('create an appointment', () => {
-    const startsAt = new Date();
-    const endsAt = new Date();
-
-    endsAt.setDate(endsAt.getDate() + 1);
+    const startsAt = getFutureDate('2023-01-15');
+    const endsAt = getFutureDate('2023-01-16');
 
     const appointment = new Appointment({
         customer: "John Doe",
@@ -19,10 +18,9 @@ test('create an appointment', () => {
 
 
 test('cannot create an appointment with end date before start date', () => {
-    const startsAt = new Date();
-    const endsAt = new Date();
+    const startsAt = getFutureDate('2023-01-15');
+    const endsAt = getFutureDate('2023-01-14');
 
-    endsAt.setDate(endsAt.getDate() - 1);
     expect(() => {
         return new Appointment({
             customer: "John Doe",
--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { Appointment } from "../entities/appointment";
+import { getFutureDate } from "../tests/utils/get-future-date";
 import { CreateAppointment } from "./create-appointment";
 
 describe('Create Appointment', () => {
@@ -7,10 +8,8 @@ describe('Create Appointment', () => {
     it('should be able to create an appointment', () => {
         const createAppointment = new CreateAppointment();
 
-        const startsAt = new Date();
-        const endsAt = new Date();
-    
-        endsAt.setDate(endsAt.getDate() + 1);
+        const startsAt = getFutureDate('2023-01-15');
+        const endsAt = getFutureDate('2023-01-16');
 
         expect(createAppointment.execute({
             customer: 'John Doe',
